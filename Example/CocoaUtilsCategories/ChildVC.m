954c3895b0e6f10f20091f10afa0c3a71144302d
koichi yamamoto <koichi@dividual.jp>
2015-02-26 14:24:43 +0900

    a

---
+++ Example/CocoaUtilsCategories/ChildVC.m
@@ -0,0 +1,55 @@
+//
+//  ChilvVC.m
+//  CocoaUtilsCategories
+//
+//  Created by noughts on 2015/02/26.
+//  Copyright (c) 2015年 koichi yamamoto. All rights reserved.
+//
+
+#import "ChildVC.h"
+#import <UIViewController+NNUtils.h>
+#import <NBULog.h>
+
+@implementation ChildVC
+
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	NBULogVerbose(@"viewDidLoad");
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+	[super viewWillAppear:animated];
+	NBULogVerbose(@"viewWillAppear");
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+	[super viewDidAppear:animated];
+	NBULogVerbose(@"viewDidAppear");
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+	[super viewWillDisappear:animated];
+	NBULogVerbose(@"viewWillDisappear");
+}
+
+-(void)viewDidDisappear:(BOOL)animated{
+	[super viewDidDisappear:animated];
+	NBULogVerbose(@"viewDidDisappear");
+}
+
+-(void)dealloc{
+	NBULogVerbose(@"dealloc");
+}
+
+
+
+
+
+
+-(IBAction)onCloseButtonTap:(id)sender{
+	[self removeFromContainerControllerAnimated:YES];
+}
+
+
+@end
---
+++ Example/CocoaUtilsCategories/UIViewControllerDemoVC.m
@@ -0,0 +1,57 @@
+//
+//  UIViewControllerDemo.m
+//  CocoaUtilsCategories
+//
+//  Created by noughts on 2015/02/26.
+//  Copyright (c) 2015年 koichi yamamoto. All rights reserved.
+//
+
+#import "UIViewControllerDemoVC.h"
+#import <UIViewController+NNUtils.h>
+#import <NBULog.h>
+
+@implementation UIViewControllerDemoVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	NBULogVerbose(@"viewDidLoad");
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+	[super viewWillAppear:animated];
+	NBULogVerbose(@"viewWillAppear");
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+	[super viewDidAppear:animated];
+	NBULogVerbose(@"viewDidAppear");
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+	[super viewWillDisappear:animated];
+	NBULogVerbose(@"viewWillDisappear");
+}
+
+-(void)viewDidDisappear:(BOOL)animated{
+	[super viewDidDisappear:animated];
+	NBULogVerbose(@"viewDidDisappear");
+}
+
+-(void)dealloc{
+	NBULogVerbose(@"dealloc");
+}
+
+
+-(IBAction)openFromSelf:(id)sender{
+	UIViewController* vc = [self.storyboard instantiateViewControllerWithIdentifier:@"ChildVC"];
+	[self addContentController:vc animated:YES];
+}
+
+
+-(IBAction)openFromNavigationController:(id)sender{
+	UIViewController* vc = [self.storyboard instantiateViewControllerWithIdentifier:@"ChildVC"];
+	[self.navigationController addContentController:vc animated:YES];
+}
+
+
+@end
